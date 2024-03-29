ab4b85941aa4304112dea043da1fe3044574068e
Pedro Neres <bypedroneres@gmail.com>
2024-03-29 14:44:12 -0300

    changed the boxing on hero page

--- athlete/src/components/Hero.js
+++ athlete/src/components/Hero.js
@@ -5,10 +5,10 @@ function Hero() {
   return (
     <div className='hero'>
         <div className='hero_Content'>
-            <h1>
-                UNLEASH YOUR POTENTIAL
-            </h1>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <div className='hero_Text'>
+          <h1>UNLEASH YOUR POTENTIAL</h1>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
         </div>
     </div>
   )
