363e4578d09e5389f473414edc152a63014cdd42
Pedro Neres <pedro.neres@spyskytech.com>
2024-04-17 11:35:27 -0300

    added the link to founder

--- athlete/src/components/Founder.js
+++ athlete/src/components/Founder.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import "../components/Founder.css"
+import React from 'react';
+import '../components/Founder.css';
 
 function Founder() {
   return (
     <div className='founder'>
-        <div className='founder_Content'>
-            <div className='founder_Card'>
-                <div className='founder_Text'>
-                    <h2>THE FOUNDER'S STORY</h2>
-                    <p>Our founder, Luis, learned firsthand the consequences of overlooking non-sport-specific training. Today, he is committed to ensuring that younger generations don’t repeat the same mistakes.</p>
-                <div className='founder_Buttons'>
-                    <div className='founder_Button'>
-                        <button>
-                        Learn more
-                        </button>
-                    </div>
-                </div>
-                </div>
+      <div className='founder_Content'>
+        <div className='founder_Card'>
+          <div className='founder_Text'>
+            <h2>THE FOUNDER'S STORY</h2>
+            <p>
+              Our founder, Luis, learned firsthand the consequences of
+              overlooking non-sport-specific training. Today, he is committed
+              to ensuring that younger generations don’t repeat the same
+              mistakes.
+            </p>
+            <div className='founder_Buttons'>
+              <div className='founder_Button'>
+                <a href='/founder'>
+                  <button>Learn more</button>
+                </a>
+              </div>
             </div>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Founder
+export default Founder;
