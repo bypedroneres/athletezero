d18b1e89f1397221d7ceb8075d0bacb1b1204e39
Pedro Neres <pedro.neres@spyskytech.com>
2024-04-20 13:43:45 -0300

    updated the photos of partners

--- athlete/src/components/Partners.js
+++ athlete/src/components/Partners.js
@@ -5,13 +5,13 @@ function Partners() {
   return (
     <div className='partners'>
         <div className='partners_Content'>
-        <img src='/images/partners/barcelona.png' alt='Barcelona FC' />
-        <img src='/images/partners/TCU.png' alt='TCU' />
-        <img src='/images/partners/ND.png' alt='ND' />
-        <img src='/images/partners/P.png' alt='Princeton' />
-        <img src='/images/partners/A.png' alt='A' />
-        <img src='/images/partners/AtpTour.png' alt='Atp Tour' />
-        <img src='/images/partners/UCF.png' alt='Univecity of California' />
+        <img src='/images/partners/image 20.png' alt='Barcelona FC' />
+        <img src='/images/partners/image 28.png' alt='TCU' />
+        <img src='/images/partners/image 22.png' alt='ND' />
+        <img src='/images/partners/image 23.png' alt='Princeton' />
+        <img src='/images/partners/image 24.png' alt='A' />
+        <img src='/images/partners/image 26.png' alt='Atp Tour' />
+        <img src='/images/partners/image 27.png' alt='Univecity of California' />
         </div>
     </div>
   )
