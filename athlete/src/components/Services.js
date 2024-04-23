9d5dd54909c5059c5e7faa2b7def5318f34cc2dd
Pedro Neres <pedroneres@Pedros-MacBook-Air.local>
2024-04-22 21:04:18 -0300

    added links to the services

--- athlete/src/components/Services.js
+++ athlete/src/components/Services.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import "../components/Services.css"
+import React from 'react';
+import "../components/Services.css";
 
 function Services() {
   return (
     <div className='services'>
-        <div className='services_Content'>
-            <h2>SERVICES</h2>
-            <div className='services_Cards'>
-                <div className='services_Card-1'>
-                <div className='services_Text'>
-                    <h3>MENTAL COACHING</h3>
-                    <p>The greatest athletes are characterized by their ability to control their minds. Learning how to deal with pressure, reduce anxiety, and flow in the present is key to any athlete's success.</p>
-                </div>
-                </div>
-                <div className='services_Card-2'>
-                    <div className='services_Text'>
-                    <h3>NUTRITION</h3>
-                    <p>Generic nutritional advice doesn’t set anyone apart; to be successful, you have to be different, you have to be the best. Comprehending the specific foods and their optimal timing based on your body's needs is crucial for an athlete's success.</p>
-                </div>
-                </div>
+      <div className='services_Content'>
+        <h2>SERVICES</h2>
+        <div className='services_Cards'>
+          <a href="/mental" className='services_Card-1'>
+            <div className='services_Text'>
+              <h3>MENTAL COACHING</h3>
+              <p>The greatest athletes are characterized by their ability to control their minds. Learning how to deal with pressure, reduce anxiety, and flow in the present is key to any athlete's success.</p>
             </div>
+          </a>
+          <a href="/nutrition" className='services_Card-2'>
+            <div className='services_Text'>
+              <h3>NUTRITION</h3>
+              <p>Generic nutritional advice doesn’t set anyone apart; to be successful, you have to be different, you have to be the best. Comprehending the specific foods and their optimal timing based on your body's needs is crucial for an athlete's success.</p>
+            </div>
+          </a>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
