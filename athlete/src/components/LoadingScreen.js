8ca2126c63a0ebac974a7737cf22ec216cb46e41
Pedro Neres <pedroneres@Pedros-MacBook-Air.local>
2024-04-23 09:42:24 -0300

    fixed loading bug

--- athlete/src/components/LoadingScreen.js
+++ athlete/src/components/LoadingScreen.js
@@ -15,9 +15,11 @@ const LoadingScreen = () => {
   }, []); // Empty dependency array ensures useEffect runs only once
 
   return (
-    <div className="loading-screen" style={{ opacity: isLoading ? 1 : 0, transition: 'opacity 0.5s ease' }}>
-      <img src='/images/loading/logo.svg' alt='Phone' />
-    </div>
+    isLoading && (
+      <div className="loading-screen" style={{ opacity: isLoading ? 1 : 0, transition: 'opacity 0.5s ease' }}>
+        <img src='/images/loading/logo.svg' alt='Phone' />
+      </div>
+    )
   );
 };
 
