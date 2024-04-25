0a18a0f1dc0cff96c3419986212c36ceee4d8d8e
Pedro Neres <pedroneres@Pedros-MacBook-Air.local>
2024-04-25 13:24:31 -0300

    finished form

--- athlete/src/components/About.js
+++ athlete/src/components/About.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import "../components/About.css";
+import React, { useState } from 'react';
+import '../components/About.css';
+import GetStartedPopUp from '../components/GetStartedPopUp'; // Import the GetStartedPopUp component
 
 function About() {
-  // Link to the Google Form
-  const googleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSchkoH17qTzbaQ-t_rHl6ATJ38C95i9eMBBUNn9pRvGW6FSGw/viewform";
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className='about'>
-        <div className='about_Content'>
-            <div className='about_Image'>
-            <img src='/images/phone-1.png' alt='Phone' />
-            </div>
-            <div className='about_Text'>
-                <h2>PHILOSOPHY:</h2>
-                <p>The only thing that all athletes have in common is that they are all different. Every single athlete has different needs, goals, motivations… We treat each athlete as unique, adapting our approach to provide exactly what they need.</p>
-                <a href={googleFormLink} target="_blank" rel="noopener noreferrer">
-                    <button>
-                        Get Started
-                    </button>
-                </a>
-            </div>
-        </div>      
+      <div className='about_Content'>
+        <div className='about_Image'>
+          <img src='/images/phone-1.png' alt='Phone' />
+        </div>
+        <div className='about_Text'>
+          <h2>PHILOSOPHY:</h2>
+          <p>The only thing that all athletes have in common is that they are all different. Every single athlete has different needs, goals, motivations… We treat each athlete as unique, adapting our approach to provide exactly what they need.</p>
+          <button onClick={openPopup}>Get Started</button>
+        </div>
+      </div>
+      {showPopup && <GetStartedPopUp onClose={closePopup} />}
     </div>
   );
 }
---
+++ athlete/src/components/GetStartedPopUp.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import '../components/PopUp.css';
+import firestore from '../firebase'; // Import the Firestore instance
+
+function GetStartedPopUp({ onClose }) {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    age: '',
+    sport: '',
+    servicesInterest: '',
+    time: '', // Add time field to formData state
+    discountCode: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await firestore.collection('getStarted').add(formData); // Send form data to Firestore
+      console.log('Form submitted:', formData);
+      onClose(); // Close the popup
+      alert('Get Started form submitted successfully!');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      alert('Error submitting Get Started form. Please try again later.');
+    }
+  };
+
+  return (
+    <div className='popup'>
+      <div className='popup_Content'>
+        <form onSubmit={handleSubmit}>
+          <h4>Get Started Form</h4>
+          <label>
+            <input
+              type='text'
+              placeholder='E-mail:'
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Athletes FullName:'
+              name='fullName'
+              value={formData.fullName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Athletes Age:'
+              name='age'
+              value={formData.age}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Athletes Sport:'
+              name='sport'
+              value={formData.sport}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+          <select
+            name='servicesInterest'
+            value={formData.servicesInterest}
+            onChange={handleChange}
+            required
+          >
+            <option value=''>Select Services</option>
+            <option value='mental coaching'>Mental Coaching</option>
+            <option value='nutrition'>Nutrition</option>
+            <option value='both'>Both</option>
+          </select>
+        </label>
+          <label>
+            <input
+              type='text'
+              placeholder='List 3 available times for an online session:'
+              name='time'
+              value={formData.time}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Discount Code:'
+              name='discountCode'
+              value={formData.discountCode}
+              onChange={handleChange}
+            />
+          </label>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default GetStartedPopUp;
